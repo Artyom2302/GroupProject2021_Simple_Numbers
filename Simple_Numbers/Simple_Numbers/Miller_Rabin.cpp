498ce16d474a5d6f461bfbd55f9309178dec6db7
PashkovDanilASU <properina45@gmail.com>
2021-11-23 17:13:03 +0300

    Update withou vector bibibibibibavbbababababa

--- Simple_Numbers/Simple_Numbers/Miller_Rabin.cpp
+++ Simple_Numbers/Simple_Numbers/Miller_Rabin.cpp
@@ -2,42 +2,44 @@
 #include <cmath>
 #include <random>
 
-vector <int> Miller_Rabin(vector <int> massivich) {
-	cout << "How much iterations are you want";
-	cin >> k;
-	vector <int> results;
-	for (const auto& item : massivich) {
-		if (Test_Millera_Rabina(item, k)) {
-			results.push_back(item);
-		}
+template <typename T>
+T modpow(T base, T exp, T modulus) {
+	base %= modulus;
+	T result = 1;
+	while (exp > 0) {
+		if (exp & 1) result = (result * base) % modulus;
+		base = (base * base) % modulus;
+		exp >>= 1;
 	}
+	return result;
 }
 
-bool Test_Millera_Rabina(long long n, int k) {
+bool Test_Millera_Rabina(int n, int k) {
 	if (n == 2 || n == 3) {
 		return true;
 	}
 	if (n < 2 || n % 2 == 0) {
 		return false;
 	}
-	long long t = n - 1;
+	int t = n - 1;
 	int s = 0;
 	while (t % 2 == 0) {
 		t /= 2;
 		++s;
 	}
 	for (int i = 0; i < k; ++i) {
-		long long a;
+		int a;
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<unsigned long long> dis(2, n-2);
+		//rand(); 32767
 		a = dis(gen);
-		long long x = pow(x, t) % n;
+		int x = modpow(a, t, n);
 		if (x == 1 || x = n - 1) {
 			continue;
 		}
 		for (int r = 1; r < s; ++r) {
-			x = pow(x, 2) % n;
+			x = modpow(x, (int)2, n);
 			if (x == 1) {
 				return false;
 			}
