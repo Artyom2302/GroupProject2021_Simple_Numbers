e86b44fdd07f701a450aa99bbdb1fe182463001c
aislu1 <aisly.agdavletova@gmail.com>
2021-11-23 20:17:22 +0300

    Eratosfen

--- Simple_Numbers/Simple_Numbers/Eratosfen.cpp
+++ Simple_Numbers/Simple_Numbers/Eratosfen.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 vector <int> Eratosfen(vector <int> mas)
 {
+	vector <int> massiv;
+	massiv.reserve(mas.size());
+	mas[1] = 0;
 	for (int p = 2; p < mas.size(); p++)
 	{
 		if (mas[p] != 0)
 		{
-			cout << mas[p] << endl;
 			for (int j = p * p; j < mas.size(); j += p)
 				mas[j] = 0;
 		}
 	}
+	for (int i = 0; i < mas.size(); i++) {
+		if (mas[i] != 0) {
+			massiv.push_back(mas[i]);
+		}
+	}
 
-	return mas;
+	return massiv;
 }
